f4f31927039c2d9658bac12e2a59690e52a933ed
Christopher Davis <cdavis9999@gmail.com>
2015-09-09 19:59:51 -0400

    Stub Out the Testing Stuff

---
+++ test/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of pmg/three-repositories
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace PMG\ThreeRepositories;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of pmg/three-repositories
+ *
+ * Copyright (c) PMG <https://www.pmg.com>
+ *
+ * For full copyright information see the LICENSE file distributed
+ * with this source code.
+ *
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+$loader->addPsr4('PMG\\ThreeRepositories\\', __DIR__);
